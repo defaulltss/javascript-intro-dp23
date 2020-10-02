2aa2f65bbd023141681a7be03419e1bb6fa7cf3b
[Students] Ralfs Daniels Uks <93DPRDUks@rvt.lv>
2020-10-02 09:37:22 +0300

    4th

---
+++ javascript2/js/script.js
@@ -0,0 +1,14 @@
+console.log("Loop task")
+
+function printHello(){
+    console.log("sodien nav super diena, es neesmu preicigs")
+
+
+}
+
+function showPattern(totalRows){
+    for(let curRow = 1; curRow <= totalRows; curRow++){
+        console.log(curRow)
+    }
+
+}
